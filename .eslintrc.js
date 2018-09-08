7c323809a87f66714758a0776a453dbfa8701300
Kevin Marren <kmarren@signal.co>
2018-09-08 11:17:45 -0500

    add a failing test for the first use case

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: "airbnb-base",
+  plugins: ["jasmine"],
+  env: { jasmine: true}
+};
---
+++ spec/useCases.Spec.js
@@ -0,0 +1,7 @@
+describe('Use Cases', () => {
+  describe('Use Case 1', () => {
+    it('An Admin loads a VendingMachine with 5 Twixes (price: $.75 each), 10 Sour Patch Kids (price: $2.00 each), and 3 Atomic Warheads (price: $.50 each)', () => {
+      expect(true).toBe(false);
+    });
+  });
+});
